2e13d1f659bb1da956a3dea219a1052692c06d91
Anurag Kesari <akkesari@wpi.edu>
2015-04-19 18:33:40 -0400

    Second test commit to github

--- akkesari_Nestor/src/akkesari/Nestor.java
+++ akkesari_Nestor/src/akkesari/Nestor.java
@@ -5,14 +5,25 @@ import ks.common.model.BuildablePile;
 import ks.common.model.Column;
 import ks.common.model.Deck;
 import ks.common.model.Pile;
+import ks.common.view.BuildablePileView;
+import ks.common.view.ColumnView;
+import ks.common.view.DeckView;
+import ks.common.view.IntegerView;
+import ks.common.view.PileView;
 
 public class Nestor extends Solitaire {
 
+	// Model
 	Deck deck;
 	Pile wastePile;
 	Column columns[];
 	BuildablePile reserve;
-	
+	// Views
+	DeckView deckView;
+	PileView wastePileView;
+	BuildablePileView reserveView;
+	ColumnView[] columnView;
+	IntegerView scoreView;
 	
 	@Override
 	public String getName() {
@@ -26,6 +37,29 @@ public class Nestor extends Solitaire {
 
 	@Override
 	public void initialize() {
+		// initialize model
+		initializeModel(getSeed());
+		initializeView();
+		initializeControllers();
+
+		// prepare game by creating the deck etc
+
+		updateScore(0);
+		updateNumberCardsLeft (52);
+		
+	}
+
+	private void initializeControllers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initializeView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initializeModel(int seed) {
 		// TODO Auto-generated method stub
 		
 	}
