d2a980c426ca416e80c86f1ff19efbb61827101c
Anurag Kesari <akkesari@wpi.edu>
2015-04-19 20:30:55 -0400

    View loads and shows cards. Requires better layout.

--- akkesari_Nestor/src/akkesari/Nestor.java
+++ akkesari_Nestor/src/akkesari/Nestor.java
@@ -11,20 +11,22 @@ import ks.common.view.ColumnView;
 import ks.common.view.DeckView;
 import ks.common.view.IntegerView;
 import ks.common.view.PileView;
+import ks.launcher.Main;
 
 public class Nestor extends Solitaire {
 
 	// Model
 	Deck deck;
 	Pile wastePile;
-	Column columns[];
+	Column columns[] = new Column[9];
 	BuildablePile reserve;
 	// Views
 	DeckView deckView;
 	PileView wastePileView;
 	BuildablePileView reserveView;
-	ColumnView[] columnView;
+	ColumnView columnView[] = new ColumnView[9];
 	IntegerView scoreView;
+	IntegerView numLeftView;
 	
 	@Override
 	public String getName() {
@@ -58,15 +60,39 @@ public class Nestor extends Solitaire {
 		// Initialize the view
 		CardImages ci = getCardImages();
 
+		// Create deck view
 		deckView = new DeckView (deck);
 		deckView.setBounds (20,20, ci.getWidth(), ci.getHeight());
 		container.addWidget (deckView);
-
-		// create BuildablePileViews, one after the other (default to 13 full cards -- more than we'll need)
 		
+		// Create waste pile view
 		wastePileView = new PileView(wastePile);
-		pileViews[pileNum].setBounds (20*pileNum + (pileNum-1)*ci.getWidth(), ci.getHeight() + 50, ci.getWidth(), 13*ci.getHeight());
-		container.addWidget (pileViews[pileNum]);
+		wastePileView.setBounds(20 + 10 * ci.getWidth(), 20 + 2 * ci.getHeight(), ci.getWidth(), ci.getHeight());
+		container.addWidget(wastePileView);
+		
+		// Create reserve pile view
+		reserveView = new BuildablePileView(reserve);
+		reserveView.setBounds(20 + 3 * ci.getWidth(), 20 + 2 * ci.getHeight(), ci.getWidth(), ci.getHeight());
+		container.addWidget(reserveView);
+		
+		// Create the 8 column views
+		for(int i = 1; i <= 8; i++){
+			columnView[i] = new ColumnView(columns[i]);
+			columnView[i].setBounds(20 + i * ci.getWidth(), 20, ci.getWidth(), ci.getHeight());
+			container.addWidget(columnView[i]);
+		}
+		
+		// Create the score view
+		scoreView = new IntegerView (getScore());
+		scoreView.setFontSize(14);
+		scoreView.setBounds (20 + 3 * ci.getWidth(), 20 + 4 * ci.getHeight(), 160, 60);
+		container.addWidget (scoreView);
+
+		// Create the cards left view
+		numLeftView = new IntegerView (getNumLeft());
+		numLeftView.setFontSize(14);
+		numLeftView.setBounds (20 + 6 * ci.getWidth(), 20 + 4 * ci.getHeight(), 160, 60);
+		container.addWidget (numLeftView);
 		
 	}
 
@@ -80,8 +106,8 @@ public class Nestor extends Solitaire {
 		wastePile = new Pile("waste");
 		model.addElement(wastePile);
 		
-		// Create the 4 columns and add them to the model.
-		for(int i = 1; i<=4; i++){
+		// Create the 8 columns and add them to the model.
+		for(int i = 1; i<=8; i++){
 			columns[i] = new Column("column" + i);
 			model.addElement(columns[i]);
 		}
@@ -94,7 +120,13 @@ public class Nestor extends Solitaire {
 		updateScore(0);
 		updateNumberCardsLeft (52);
 		
-		
+	}
+	
+	/** Code to launch solitaire variation. */
+	public static void main (String []args) {
+		// Seed is to ensure we get the same initial cards every time.
+		// Here the seed is to "order by suit."
+		Main.generateWindow(new Nestor(), Deck.OrderBySuit);
 	}
 
 }
